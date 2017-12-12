113c5af7612bcefd0b76996bcf9e4e738e327ab9
Daniel <34325314+daniel0br@users.noreply.github.com>
2017-12-12 07:17:47 -0300

    Detect Items to be tested
    Added function to detect the items that should be tested

--- Assets/Scripts/Rules.cs
+++ Assets/Scripts/Rules.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Rules : MonoBehaviour {
 
-	public List<Item> itemsToTest; //letf id public to show on debug
+	private List<Item> itemsToTest; //letf id public to show on debug
 	private List<Box> checkedBoxes;
 	private GameResourcesManager grm;
 
@@ -13,45 +14,101 @@ public class Rules : MonoBehaviour {
 	}
 
 	//class that will be used to test the rules
-	private class Item {
-		public Box box;
+	private class Item : IEquatable<Item>{
+		public Box itemBox;
 		public bool isTrueNextStep = false;
 
 		public Item (Box box){
-			this.box = box;
+			this.itemBox = box;
 			isTrueNextStep = false;
 		}
+
+		//IEquatable override methods
+		public override bool Equals(object obj)
+		{
+			if (obj == null) {
+				return false;
+			}
+
+			Item objAsItem = obj as Item;
+
+			if (objAsItem == null) {
+				return false;
+			} else {
+				return Equals (objAsItem);
+			}
+		}
+
+		//copy the hash from the box.
+		public override int GetHashCode()
+		{
+			return itemBox.GetHashCode();
+		}
+
+		public bool Equals(Item other)
+		{
+			if (other == null) {
+				return false;
+			}
+			return this.itemBox.Equals(other.itemBox);
+		}
 	}
+
+
+
+	//Execute Steps
+
 
 	//Execute one step
 	public void doStep(){
 		itemsToTest = new List<Item> ();
 		findSteps();
+		Debug.Log ("counter:" + itemsToTest.Count);
 	//	executeSteps();
 	}
 
 	private void findSteps (){
+		checkedBoxes = grm.checkedBoxes;
 		if (checkedBoxes != null) {
-			checkedBoxes = grm.checkedBoxes;
 
 			foreach (Box box in checkedBoxes) {
 				addItemToList (box);
+				getBoxNeighbors (box);
 			}
 				
 		}
 
-	//	checkedItems = 
 	}
 
 	private void addItemToList(Box box){
 		Item item = new Item (box);
-		if(!itemsToTest.Exists (x => (x.box.positionX == box.positionX &&
-								      x.box.positionY == box.positionY))){
+		if(!itemsToTest.Exists (x => (x.itemBox.positionX == box.positionX &&
+									  x.itemBox.positionY == box.positionY))){
 			itemsToTest.Add(item);
 		}
 	}
 
-	private List<Box> getBoxNeighbors(Box box){
+	private void getBoxNeighbors(Box box){
+		int tempPositionX = box.positionX;
+		int tempPositionY = box.positionY;
+
+		if(tempPositionY -1 >= 0 && tempPositionX -1 >= 0)
+			addItemToList(transform.Find ("BOX" + (tempPositionY -1) + "" + (tempPositionX -1)).GetComponent<Box>());
+		if(tempPositionY -1 >= 0)
+			addItemToList(transform.Find ("BOX" + (tempPositionY -1) + "" + tempPositionX).GetComponent<Box>());
+		if(tempPositionY -1 >= 0 && tempPositionX +1 < grm.quantityOfCollumns)
+			addItemToList(transform.Find ("BOX" + (tempPositionY -1) + "" + (tempPositionX + 1)).GetComponent<Box>());
+		if(tempPositionX -1 >= 0)
+			addItemToList(transform.Find ("BOX" + tempPositionY  + "" + (tempPositionX - 1)).GetComponent<Box>());
+		if(tempPositionX +1 < grm.quantityOfCollumns)
+			addItemToList(transform.Find ("BOX" + tempPositionY  + "" + (tempPositionX + 1)).GetComponent<Box>());
+		if(tempPositionY +1 < grm.quantityOfLines && tempPositionX -1 >= 0)
+			addItemToList(transform.Find ("BOX" + (tempPositionY +1) + "" + (tempPositionX - 1)).GetComponent<Box>());
+		if(tempPositionY +1 < grm.quantityOfLines)
+			addItemToList(transform.Find ("BOX" + (tempPositionY +1) + "" + tempPositionX ).GetComponent<Box>());
+		if(tempPositionY +1 < grm.quantityOfLines && tempPositionX +1 < grm.quantityOfCollumns)
+			addItemToList(transform.Find ("BOX" + (tempPositionY +1) + "" + (tempPositionX + 1)).GetComponent<Box>());
+
 	}
 
 	//RULES
